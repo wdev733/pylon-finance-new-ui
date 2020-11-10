6b0fb8c1389c68d9cd4e1a2a97f97d995275e62b
wdev733 <wdev733@gmail.com>
2020-11-09 19:47:47 -0500

    add font, fix header style

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./styles.scss";
+
 export default function Header() {
   return (
     <>
       <header className="header">
         <div className="header__wrapper">
+          <Link className="header__logo" to="/staging/home">
+            <img
+              className="header__logo__img"
+              src={require("../../../assets/images/pylon.png")}
+              alt="pylon - new"
+            />
+          </Link>
           <nav className="header__nav">
             <ul>
-              <li>
-                <Link className="header__logo" to="/staging/home">
-                  <img
-                    className="header__logo__img"
-                    src={require("../../../assets/images/pylon.png")}
-                    alt="pylon - new"
-                  />
-                </Link>
-              </li>
               <li className="menu-link">
                 <Link to="/staging/home">HOME</Link>
               </li>
               <li className="menu-link">
                 <Link to="/staging/about-pylon">ABOUT PYLON</Link>
               </li>
-              <li className="menu-link">
+              {/* <li className="menu-link">
                 <Link to="/staging/ycrv-vault">YCRV VAULT</Link>
-              </li>
+              </li> */}
               <li className="menu-link">
                 <Link to="/staging/fdi-vault">FDI VAULT</Link>
               </li>
